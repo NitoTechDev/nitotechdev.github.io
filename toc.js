efcc0872cbbac73c7c0c04b9c2b017963c614c37
NitoTech <nitotechdev@gmail.com>
2026-01-26 16:12:14 -0500

    CSS Update
    Added a Table of Contents feature to the right side of the screen.

---
+++ toc.js
@@ -0,0 +1,81 @@
+/*
+
+I need a way to constantly keep track of parent headings and what children should be linked to them that aren't part of the blog itself.
+
+So I need:
+    - Keep track of all headings within the blog post
+        - Keep track of what level they are (The number from the respective h1/2/3)
+            - <heading>.tagName[1] Can keep track of the respective numbers
+            - <heading>.textContent can keep track of the name of the section
+    
+    DONT DO UNTIL FIRST PART IS DONE!!!
+    - Keep an extra file or a dictionary within this JS file that keeps track of:
+        Parent ID
+        Respective Children that it should link to
+            Their respective file links
+*/
+
+let tocDisplayedHeadings = []
+
+document.addEventListener("DOMContentLoaded", () => {
+    const blogContainer = document.querySelector(".blog-main"); // Makes it so we only get the headings from within .blog-main
+    const blogHeadings = blogContainer.querySelectorAll("h1, h2, h3"); // Gets the h1 - h3 headings. 
+    createTOC(blogHeadings);
+    renderTOC(tocDisplayedHeadings);
+})
+
+function createTOC(headings) {
+    headings.forEach((heading) => {
+        const node = {
+            text: heading.textContent,
+            level: heading.tagName[1],
+            children: [],
+            heading: heading
+        }
+        tocDisplayedHeadings.push(node);
+    })
+}
+
+function renderTOC(displayedHeadings) {
+    const toc = document.getElementById("toc");
+    const tocListContainer = document.getElementById("toc-list");
+    let mostRecentUL = tocListContainer;
+    let mostRecentDetails = null;
+    displayedHeadings.forEach((heading, index) => {
+        const li = document.createElement("li");
+        
+        switch (heading.level) {
+            case "1":
+                const p = document.createElement("p");
+                p.textContent = heading.text
+                toc.prepend(p);
+                // const ul = document.createElement("ul");
+                // tocListContainer = ul;
+                // toc.appendChild(ul)
+                break;
+            case "2":
+                let hasSubheading = null
+                try { if (displayedHeadings[index + 1].level === "3") {hasSubheading = true} else { hasSubheading = false} } catch (TypeError) {hasSubheading = false}
+                if (hasSubheading) {
+                    mostRecentDetails = document.createElement("details");
+                    // mostRecentDetails.open = true; // May have to remove later.
+                    const summary = document.createElement("summary");
+                    tocListContainer.appendChild(li);
+                    li.appendChild(mostRecentDetails);
+                    summary.textContent = heading.text;
+                    mostRecentDetails.appendChild(summary);
+                    mostRecentUL = document.createElement("ul");
+                    mostRecentDetails.appendChild(mostRecentUL);
+                } else {
+                    mostRecentUL = tocListContainer
+                    li.textContent = heading.text;
+                    mostRecentUL.appendChild(li);
+                }
+                break;
+            case "3":
+                li.textContent = heading.text;
+                mostRecentUL.appendChild(li);
+                break;
+        }
+  });
+}
